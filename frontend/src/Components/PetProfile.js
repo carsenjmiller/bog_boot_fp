648df5dabe8183bdb6b0d8e765e7bd9d24bd4cef
Marlene Garcia <61206258+whymarlene@users.noreply.github.com>
2020-11-25 22:19:07 -0500

    button

--- frontend/src/Components/PetProfile.js
+++ frontend/src/Components/PetProfile.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import './style.css';
-import { 
-    Link
-  } from 'react-router-dom';
+import {
+  Link
+} from 'react-router-dom';
+// get our fontawesome imports
+import { faDog, faSyringe, faInfoCircle, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const PetProfile = (pet) => {
-    const status = pet.adopted
-    return (
-        <section className={status ? "profileHide":"profile"}>
-      <img src={pet.type === "dog" ? "https://picsum.photos/350/200":"http://placekitten.com/350/200"} id="profileImage" alt={"Picture of " + pet.name}></img>
+  const status = pet.adopted
+  return (
+    <section className={status ? "profileHide" : "profile"}>
+      <img src={pet.type === "dog" ? "https://picsum.photos/350/200" : "http://placekitten.com/350/200"} id="profileImage" alt={"Picture of " + pet.name}></img>
       <div>
         <div>
+          
           <h2>{pet.name}</h2>
-          <p>{pet.breed}</p>
-          <p>{pet.status}</p>
-          <p>{pet.gender}, {pet.yearsOld} years old</p>
+          <p><FontAwesomeIcon icon={faDog} /> {pet.breed}</p>
+          <p><FontAwesomeIcon icon={faSyringe} /> {pet.status}</p>
+          <p><FontAwesomeIcon icon={faInfoCircle} /> {pet.gender}, {pet.yearsOld} years old</p>
 
         </div>
         <Link to={"/" + pet.name + '-' + pet.type} style={{ textDecoration: 'none' }}>
-            <button >View more details</button>
+          <button >View more details   <FontAwesomeIcon icon={faArrowDown} /></button>
         </Link>
       </div>
     </section>
-    )
+  )
 }
- 
+
 export default PetProfile;
