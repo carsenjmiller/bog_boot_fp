a00079a39e03a90ff531999d9897b6d9e736af43
Deepsikha Das <deepsikhad14@gmail.com>
2020-11-30 01:00:20 -0600

    fixed dog/cat stuff on webpage

--- frontend/src/Components/AdoptAPet.js
+++ frontend/src/Components/AdoptAPet.js
@@ -7,19 +7,21 @@ const AdoptAPet = (props) => {
 	React.useEffect(() => {
 		var setCurrPets2 = props.setCurrentPets;
 		fetch('http://localhost:3001/findDocuments')
-		  .then(response => response.json())
-		  .then(data => setCurrPets2(data))
-		  .catch(error => {
-			console.error(error)
-		  })
-	  }, [])
-    return (
-		<main className= "petCards">
-			<PetProfileList species={props.content[0]} />
-			<PetProfileList species={props.content[1]} /> 
+			.then(response => response.json())
+			.then(data => setCurrPets2(data))
+			.catch(error => {
+				console.error(error)
+			})
+	}, [])
+	const dogs = (props.content[0]._id === "cat") ? (props.content[1]):(props.content[0]);
+    const cats = (props.content[0]._id === "cat") ? (props.content[0]):(props.content[1]);
+	return (
+		<main className="petCards">
+			<PetProfileList species={dogs} />
+			<PetProfileList species={cats} />
 		</main>
 
-    )
+	)
 }
 
 export default AdoptAPet;
